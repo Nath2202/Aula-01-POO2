4ae34c57168cf8f3829386a6c4c654e07bca0675
150218 <150218@A1729.labinfo.facens.br>
2018-02-20 22:20:59 -0300

    Alteração 3 Aula 01

--- Aula01-POO2/src/Aula01.java
+++ Aula01-POO2/src/Aula01.java
@@ -1,8 +1,6 @@
 
 
 public class Aula01 {
-  private String nome;
-  private int idade;
   
   public String Dados(String nome, int idade) {
 	  return "Meu nome é " + nome + " e tenho "+idade+" anos de idade";
