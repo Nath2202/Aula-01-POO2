37af1131d488c9fee04c1c43999bfbd95ac9db87
150218 <150218@A1729.labinfo.facens.br>
2018-02-20 22:15:15 -0300

    Alteração Aula 01

---
+++ Aula01-POO2/src/AppAula01.java
@@ -0,0 +1,10 @@
+
+public class AppAula01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Aula01 a1 = new Aula01();
+        System.out.println(a1.Dados("Nathalia", 22));
+	}
+
+}
--- Aula01-POO2/src/Aula01.java
+++ Aula01-POO2/src/Aula01.java
@@ -1,4 +1,13 @@
+import javax.swing.Spring;
 
 public class Aula01 {
-
+  private String nome;
+  private int idade;
+  
+  public String Dados(String nome, int idade) {
+	  return "Meu nome é " + nome + " e tenho "+idade+" anos de idade";
+  }
+   
+  
+   
 }
